2164ac6088acaf62b3fa46283668fb4d4a77786a
janyb <borsoshkin@gmail.com>
2025-03-01 13:25:06 -0600

    I added Ollama to the Test.py and added conditions to the Ollama code.

--- Test.py
+++ Test.py
@@ -1,10 +1,34 @@
-message = "Hello, do you know what is 2+2?"
+import requests
+
+message = input("Please enter a message:")
 word_to_find = "short"
 instructions = "Give short response to the message: "
 words_from_text = message.split()
+print(words_from_text)
 for word in words_from_text:
     #print(word)
     if word == word_to_find:
         print(instructions + message)
+        data = {
+            "model": "mistral",
+            "prompt": instructions + message,
+            "stream": False
+        }
+        r = requests.post("http://localhost:11434/api/generate", json=data)
+
+        response = r.json()
+        response_text = response.get("response", "answer not received")
+        print(response_text)
+        break
     elif word != word_to_find:
-        print("No keyword in the message")
+        print(message)
+        data = {
+            "model": "mistral",
+            "prompt": message,
+            "stream": False
+        }
+        r = requests.post("http://localhost:11434/api/generate", json=data)
+
+        response = r.json()
+        response_text = response.get("response", "answer not received")
+        print(response_text)
